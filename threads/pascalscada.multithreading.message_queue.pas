b70ec2bdd1e64b5d868fcacf06f1813e5cb973ea
fluisgirardi <fabio@pascalscada.com>
2016-04-25 16:18:48 -0300

    ** New method. Get all messages doing a simple call

--- threads/pascalscada.multithreading.message_queue.pas
+++ threads/pascalscada.multithreading.message_queue.pas
@@ -129,9 +129,11 @@ type
    priority (messages that must be processed before others).
   }
   {$ENDIF}
-  TpSCADAMessageQueue = class(TObject)
-  private
-    FCs:TCriticalSection;
+
+  { TpSCADAThreadUnsafeMessageQueue }
+
+  TpSCADAThreadUnsafeMessageQueue = class(TObject)
+  protected
     FirstMessage:PpSCADAMsgPkg;
     NormalMsgs:PpSCADAMsgPkg;
     LastMsg:PpSCADAMsgPkg;
@@ -142,7 +144,7 @@ type
     {$ELSE}
     //: Creates the message queue.
     {$ENDIF}
-    constructor Create;
+    constructor Create; virtual;
 
     {$IFDEF PORTUGUES}
     //: Destroi um objeto de enfileiramento de mensagens.
@@ -150,6 +152,8 @@ type
     //: Destroys the message queue.
     {$ENDIF}
     destructor  Destroy; override;
+
+    function CreateNewMessagePkg(MsgID:Cardinal; wParam, lParam:Pointer; Priority:Boolean):PpSCADAMsgPkg; virtual;
 
     {$IFDEF PORTUGUES}
     {:
@@ -197,7 +201,7 @@ type
     @seealso(PostMessage)
     }
     {$ENDIF}
-    function  PeekMessage(out Msg:TpSCADAMsg; uFilterMinMsg, uFilterMaxMsg:Cardinal; Remove:Boolean):Boolean;
+    function  PeekMessage(out Msg:TpSCADAMsg; uFilterMinMsg, uFilterMaxMsg:Cardinal; Remove:Boolean):Boolean; virtual;
 
     {$IFDEF PORTUGUES}
     {:
@@ -225,7 +229,41 @@ type
     @seealso(PeekMessage)
     }
     {$ENDIF}
-    procedure PostMessage(MsgID:Cardinal; wParam, lParam:Pointer; Priority:Boolean);
+    procedure PostMessage(msg:PpSCADAMsgPkg); virtual;
+
+    {$IFDEF PORTUGUES}
+    {:
+    Retorna uma nova fila com todas as mensagens enfileiradas na fila atual.
+
+    @seealso(PeekMessage)
+    }
+    {$ELSE}
+    {:
+    Returns a new queue with all queued messages on current queue.
+
+    @seealso(PeekMessage)
+    }
+    {$ENDIF}
+    function  GetAllQueuedMessages:TpSCADAThreadUnsafeMessageQueue; virtual;
+  end;
+
+  { TpSCADAThreadSafeMessageQueue }
+
+  TpSCADAThreadSafeMessageQueue = class(TpSCADAThreadUnsafeMessageQueue)
+  private
+    FCS:TCriticalSection;
+  public
+
+    constructor Create; override;
+    destructor Destroy; override;
+
+    function PeekMessage(out Msg: TpSCADAMsg; uFilterMinMsg,
+      uFilterMaxMsg: Cardinal; Remove: Boolean): Boolean; override;
+
+    procedure PostMessage(msg: PpSCADAMsgPkg); override;
+
+    function GetAllQueuedMessages: TpSCADAThreadUnsafeMessageQueue; override;
+
   end;
 
 ResourceString
@@ -233,37 +271,27 @@ ResourceString
 
 implementation
 
-constructor TpSCADAMessageQueue.Create;
+{ TpSCADAThreadSafeMessageQueue }
+
+constructor TpSCADAThreadSafeMessageQueue.Create;
 begin
+  inherited Create;
   FCs := TCriticalSection.Create;
-  FirstMessage := nil;
-  NormalMsgs := nil;
-  LastMsg := nil;
-  FMsgCount := 0;
 end;
 
-destructor TpSCADAMessageQueue.Destroy;
-var
-  msg, nextmsg:PpSCADAMsgPkg;
+destructor TpSCADAThreadSafeMessageQueue.Destroy;
 begin
   FCs.Acquire;
   try
-    msg := FirstMessage;
-    while (msg<>nil) do begin
-      nextmsg := (msg^).NextMsg;
-      FreeMem(msg);
-      msg := nextmsg;
-    end;
+    inherited Destroy;
   finally
     FCs.Release;
   end;
   FreeAndNil(FCs);
 end;
 
-function TpSCADAMessageQueue.PeekMessage(out Msg:TpSCADAMsg; uFilterMinMsg, uFilterMaxMsg:Cardinal; Remove:Boolean):Boolean;
-var
-  curmsg, aux:PpSCADAMsgPkg;
-  found:Boolean;
+function TpSCADAThreadSafeMessageQueue.PeekMessage(out Msg: TpSCADAMsg;
+  uFilterMinMsg, uFilterMaxMsg: Cardinal; Remove: Boolean): Boolean;
 begin
   if FCs=nil then begin
      Result := false;
@@ -271,168 +299,226 @@ begin
   end;
   FCs.Acquire;
   try
-    Result := false;
-    found := false;
-    curmsg := FirstMessage;
-    while (curmsg<>nil) do begin
-      if (uFilterMinMsg<>0) and (uFilterMaxMsg<>0) then begin
-        if (curmsg^.Msg.MsgID>=uFilterMinMsg) and (curmsg^.Msg.MsgID<=uFilterMaxMsg) then begin
-          found := true;
-          break;
-        end;
-      end else begin
-        found := true;
-        break;
-      end;
-      curmsg := curmsg^.NextMsg;
-    end;
-
-    if found then begin
-      Msg := curmsg^.Msg;
-      //se a mensagem encontrada é a primeira e é para remover a mensagem da fila
-      //diz q a próxima msg é a primeira.
-      //
-      //if the message found is the first item and it will be removed from the
-      //queue, so, the next message will be the first
-      if Remove and (curmsg = FirstMessage) then
-        FirstMessage := curmsg^.NextMsg;
-
-      //se a mensagen encontrada é a última e é para remover a mensagem da fila
-      //diz q a msg anterior é a última.
-      //
-      //if the message found is the last item of the queue and it will be removed
-      //from the queue, so the last messagewill be the prior message.
-      if Remove and (curmsg = LastMsg) then
-        LastMsg := curmsg^.PriorMsg;
-
-      //se a mensagem encontrada é a primeira normal
-      //reaponta mensagens
-      //
-      //if the message found is the first normal message (without priority)
-      //and it will be removed from the queue, rebuilt the queue.
-      if Remove and (curmsg = NormalMsgs) then begin
-        NormalMsgs := curmsg^.NextMsg;
-        if NormalMsgs<>nil then
-          NormalMsgs^.PriorMsg := curmsg^.PriorMsg;
-      end;
-
-      //se existe mensagens antes dessa
-      //if exists messages prior of this
-      if Remove and (curmsg^.PriorMsg<>nil) then begin
-        aux := curmsg^.PriorMsg;
-        aux^.NextMsg := curmsg^.NextMsg
-      end;
-
-      //se existem mensagens após essa msg
-      //if exists messages after this.
-      if Remove and (curmsg^.NextMsg<>nil) then begin
-        aux := curmsg^.NextMsg;
-        aux^.PriorMsg := curmsg^.PriorMsg
-      end;
-
-      //libera a memória usada pela mensagem
-      //caso a ordem seja para remover...
-      //
-      //if the message will be removed from the queue, free the memory.
-      if Remove then begin
-        Dispose(curmsg);
-        FMsgCount := FMsgCount - 1;
-      end;
-
-      Result := true;
-    end;
+    Result:=inherited PeekMessage(Msg, uFilterMinMsg, uFilterMaxMsg, Remove);
   finally
     FCs.Release;
   end;
-
 end;
 
-procedure TpSCADAMessageQueue.PostMessage(MsgID:Cardinal; wParam, lParam:Pointer; Priority:Boolean);
-var
-  msg, aux:PpSCADAMsgPkg;
-  err:boolean;
+procedure TpSCADAThreadSafeMessageQueue.PostMessage(msg: PpSCADAMsgPkg);
 begin
-  //cria uma mensagem na memória.
-  //creates a new queue item.
-  err := false;
-  try
-     New(msg);
-     if msg=nil then
-        err := true;
-  except
-     err:=true;
-  end;
-  if err then
-     raise Exception.Create(SoutOfMemory);
+  if FCs=nil then
+    exit;
 
-  msg^.Msg.MsgID := MsgID;
-  msg^.Msg.lParam := lParam;
-  msg^.Msg.wParam := wParam;
-  msg^.Msg.Priority := Priority;
-  msg^.NextMsg := nil;
-  msg^.PriorMsg := nil;
-
-  //adiquire direiro sobre a fila.
-  // enter on queue mutex.
+  //pega o acesso a fila.
+  //enter on queue mutex.
   FCs.Acquire;
   try
-    //se a primeira msg é nula, a fila esta vazia.
-    //if the first queue item is null, the queue is empty.
-    if FirstMessage=nil then begin
-      FirstMessage := msg;
-      LastMsg := msg;
-      if not Priority then
-        NormalMsgs := msg;
-    end else begin
-      //se existem msgs na fila.
-      //if the queue is not empty.
-
-      //e se existe só uma msg
-      //if exists only one message on the queue.
-      if FirstMessage = LastMsg then begin
-        FirstMessage^.NextMsg := msg;
-        msg^.PriorMsg := FirstMessage;
-        LastMsg := msg;
-        if (not Priority) and (NormalMsgs=nil) then
-          NormalMsgs := msg;
-      end else begin
-        //se existirem duas ou mais mensagens na fila...
-        //if exists two or more messages on queue.
-        if Priority then
-          if NormalMsgs=nil then begin
-            LastMsg^.NextMsg := msg;
-            msg^.PriorMsg := LastMsg;
-            LastMsg := msg;
-          end else begin
-            aux := NormalMsgs^.PriorMsg;
-            //se é nulo, é a primeira msg
-            //if is nil, is the first message.
-            if aux=nil then
-              FirstMessage := msg
-            else
-              aux^.NextMsg :=  msg;
-            msg^.PriorMsg := aux;
-            msg^.NextMsg := NormalMsgs;
-            NormalMsgs^.PriorMsg := msg;
-          end
-        else begin
-          LastMsg^.NextMsg := msg;
-          msg^.PriorMsg := LastMsg;
-          LastMsg := msg;
-          if (NormalMsgs=nil) then
-            NormalMsgs := msg;
-        end;
-      end;
-    end;
-
-    //sinaliza que há mensagens na fila.
-    //increment the messages on queue.
-    FMsgCount := FMsgCount + 1;
+    inherited PostMessage(msg);
   finally
     //libera a fila.
     //releases the mutex.
     FCs.Release;
   end;
+end;
+
+function TpSCADAThreadSafeMessageQueue.GetAllQueuedMessages: TpSCADAThreadUnsafeMessageQueue;
+begin
+  FCs.Enter;
+  try
+    Result:=inherited GetAllQueuedMessages;
+  finally
+    FCs.Leave;
+  end;
+end;
+
+//==============================================================================
+
+constructor TpSCADAThreadUnsafeMessageQueue.Create;
+begin
+  FirstMessage := nil;
+  NormalMsgs := nil;
+  LastMsg := nil;
+  FMsgCount := 0;
+end;
+
+destructor TpSCADAThreadUnsafeMessageQueue.Destroy;
+var
+  msg, nextmsg:PpSCADAMsgPkg;
+begin
+  msg := FirstMessage;
+  while (msg<>nil) do begin
+    nextmsg := (msg^).NextMsg;
+    FreeMem(msg);
+    msg := nextmsg;
+  end;
+  inherited Destroy;
+end;
+
+function TpSCADAThreadUnsafeMessageQueue.CreateNewMessagePkg(MsgID: Cardinal;
+  wParam, lParam: Pointer; Priority: Boolean): PpSCADAMsgPkg;
+begin
+  //cria uma mensagem na memória.
+  //creates a new queue item.
+  New(Result);
+  if Result=nil then
+    raise Exception.Create(SoutOfMemory);
+
+  Result^.Msg.MsgID := MsgID;
+  Result^.Msg.lParam := lParam;
+  Result^.Msg.wParam := wParam;
+  Result^.Msg.Priority := Priority;
+  Result^.NextMsg := nil;
+  Result^.PriorMsg := nil;
+end;
+
+function TpSCADAThreadUnsafeMessageQueue.PeekMessage(out Msg:TpSCADAMsg; uFilterMinMsg, uFilterMaxMsg:Cardinal; Remove:Boolean):Boolean;
+var
+  curmsg, aux:PpSCADAMsgPkg;
+  found:Boolean;
+begin
+  Result := false;
+  found  := false;
+  curmsg := FirstMessage;
+  while (curmsg<>nil) do begin
+    if (uFilterMinMsg<>0) and (uFilterMaxMsg<>0) then begin
+      if (curmsg^.Msg.MsgID>=uFilterMinMsg) and (curmsg^.Msg.MsgID<=uFilterMaxMsg) then begin
+        found := true;
+        break;
+      end;
+    end else begin
+      found := true;
+      break;
+    end;
+    curmsg := curmsg^.NextMsg;
+  end;
+
+  if found then begin
+    Msg := curmsg^.Msg;
+    //se a mensagem encontrada é a primeira e é para remover a mensagem da fila
+    //diz q a próxima msg é a primeira.
+    //
+    //if the message found is the first item and it will be removed from the
+    //queue, so, the next message will be the first
+    if Remove and (curmsg = FirstMessage) then
+      FirstMessage := curmsg^.NextMsg;
+
+    //se a mensagen encontrada é a última e é para remover a mensagem da fila
+    //diz q a msg anterior é a última.
+    //
+    //if the message found is the last item of the queue and it will be removed
+    //from the queue, so the last messagewill be the prior message.
+    if Remove and (curmsg = LastMsg) then
+      LastMsg := curmsg^.PriorMsg;
+
+    //se a mensagem encontrada é a primeira normal
+    //reaponta mensagens
+    //
+    //if the message found is the first normal message (without priority)
+    //and it will be removed from the queue, rebuilt the queue.
+    if Remove and (curmsg = NormalMsgs) then begin
+      NormalMsgs := curmsg^.NextMsg;
+      if NormalMsgs<>nil then
+        NormalMsgs^.PriorMsg := curmsg^.PriorMsg;
+    end;
+
+    //se existe mensagens antes dessa
+    //if exists messages prior of this
+    if Remove and (curmsg^.PriorMsg<>nil) then begin
+      aux := curmsg^.PriorMsg;
+      aux^.NextMsg := curmsg^.NextMsg
+    end;
+
+    //se existem mensagens após essa msg
+    //if exists messages after this.
+    if Remove and (curmsg^.NextMsg<>nil) then begin
+      aux := curmsg^.NextMsg;
+      aux^.PriorMsg := curmsg^.PriorMsg
+    end;
+
+    //libera a memória usada pela mensagem
+    //caso a ordem seja para remover...
+    //
+    //if the message will be removed from the queue, free the memory.
+    if Remove then begin
+      Dispose(curmsg);
+      FMsgCount := FMsgCount - 1;
+    end;
+
+    Result := true;
+  end;
+end;
+
+procedure TpSCADAThreadUnsafeMessageQueue.PostMessage(msg: PpSCADAMsgPkg);
+var
+  aux:PpSCADAMsgPkg;
+begin
+  //se a primeira msg é nula, a fila esta vazia.
+  //if the first queue item is null, the queue is empty.
+  if FirstMessage=nil then begin
+    FirstMessage := msg;
+    LastMsg := msg;
+    if not msg^.Msg.Priority then
+      NormalMsgs := msg;
+  end else begin
+    //se existem msgs na fila.
+    //if the queue is not empty.
+
+    //e se existe só uma msg
+    //if exists only one message on the queue.
+    if FirstMessage = LastMsg then begin
+      FirstMessage^.NextMsg := msg;
+      msg^.PriorMsg := FirstMessage;
+      LastMsg := msg;
+      if (not msg^.Msg.Priority) and (NormalMsgs=nil) then
+        NormalMsgs := msg;
+    end else begin
+      //se existirem duas ou mais mensagens na fila...
+      //if exists two or more messages on queue.
+      if msg^.Msg.Priority then
+        if NormalMsgs=nil then begin
+          LastMsg^.NextMsg := msg;
+          msg^.PriorMsg := LastMsg;
+          LastMsg := msg;
+        end else begin
+          aux := NormalMsgs^.PriorMsg;
+          //se é nulo, é a primeira msg
+          //if is nil, is the first message.
+          if aux=nil then
+            FirstMessage := msg
+          else
+            aux^.NextMsg :=  msg;
+          msg^.PriorMsg := aux;
+          msg^.NextMsg := NormalMsgs;
+          NormalMsgs^.PriorMsg := msg;
+        end
+      else begin
+        LastMsg^.NextMsg := msg;
+        msg^.PriorMsg := LastMsg;
+        LastMsg := msg;
+        if (NormalMsgs=nil) then
+          NormalMsgs := msg;
+      end;
+    end;
+  end;
+
+  //sinaliza que há mensagens na fila.
+  //increment the messages on queue.
+  FMsgCount := FMsgCount + 1;
+end;
+
+function TpSCADAThreadUnsafeMessageQueue.GetAllQueuedMessages: TpSCADAThreadUnsafeMessageQueue;
+begin
+  Result:=TpSCADAThreadUnsafeMessageQueue.Create;
+  Result.FirstMessage := self.FirstMessage;
+  Result.NormalMsgs   := self.NormalMsgs;
+  Result.LastMsg      := self.LastMsg;
+  Result.FMsgCount    := self.FMsgCount;
+
+  self.FirstMessage := nil;
+  self.NormalMsgs   := nil;
+  self.LastMsg      := nil;
+  self.FMsgCount    := 0;
 end;
 
 end.
