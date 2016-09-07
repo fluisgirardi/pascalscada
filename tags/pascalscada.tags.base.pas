d8d2d1f920b2618be9bf7df5395290e7fc0fc508
fluisgirardi <fabio@pascalscada.com>
2016-09-07 02:35:23 -0300

    ** Tags

--- tags/pascalscada.tags.base.pas
+++ tags/pascalscada.tags.base.pas
@@ -5,9 +5,18 @@ unit pascalscada.tags.base;
 interface
 
 uses
-  Classes, SysUtils, fgl;
+  Classes, SysUtils, fgl, pascalscada.utilities.scales.basescale;
 
 type
+
+  TpSCADATagValueState = (ioNone,ioOk,ioDriverError, ioCommError, ioTimeOut,
+                          ioIllegalFunction, ioIllegalRegAddress,ioIllegalValue,
+                          ioPLCError, ioTagError, ioNullDriver, ioIllegalRequest,
+                          ioIllegalStationAddress, ioObjectNotExists,
+                          ioIllegalMemoryAddress, ioUnknownError, ioEmptyPacket,
+                          ioPartialOk, ioAcknowledge, ioBusy, ioNACK,
+                          ioMemoryParityError, ioGatewayUnavailable,
+                          ioDeviceGatewayFailedToRespond);
 
   TpSCADATagDataType = (tdtBool,
                         tdtUInt8,  tdtInt8,
@@ -26,9 +35,21 @@ type
     FValidadePropertyChange: TpSCADAValidatePropertyChange;
     procedure AddressChanged; virtual; abstract;
     procedure DoChangeValidation(PropertyNumber:Word; OldValue, NewValue:Int64); virtual;
+  public
+    procedure Assign(Source:TpSCADAAddressInfo); virtual;
   published
     property ValidadePropertyChange:TpSCADAValidatePropertyChange read FValidadePropertyChange write FValidadePropertyChange;
   end;
+
+  TpSCADAGetIntegerValue  = procedure(const addresInfo:TpSCADAAddressInfo; var Value:Int64;     var ValueTimeStamp:TDateTime; var ValueQuality: TpSCADATagValueState) of object;
+  TpSCADAGetStringValue   = procedure(const addresInfo:TpSCADAAddressInfo; var Value:String;    var ValueTimeStamp:TDateTime; var ValueQuality: TpSCADATagValueState) of object;
+  TpSCADAGetDoubleValue   = procedure(const addresInfo:TpSCADAAddressInfo; var Value:Double;    var ValueTimeStamp:TDateTime; var ValueQuality: TpSCADATagValueState) of object;
+  TpSCADAGetDateTimeValue = procedure(const addresInfo:TpSCADAAddressInfo; var Value:TDateTime; var ValueTimeStamp:TDateTime; var ValueQuality: TpSCADATagValueState) of object;
+
+  TpSCADASetIntegerValue  = procedure(const addresInfo:TpSCADAAddressInfo; const Value:Int64;     var ValueQuality: TpSCADATagValueState) of object;
+  TpSCADASetStringValue   = procedure(const addresInfo:TpSCADAAddressInfo; const Value:String;    var ValueQuality: TpSCADATagValueState) of object;
+  TpSCADASetDoubleValue   = procedure(const addresInfo:TpSCADAAddressInfo; const Value:Double;    var ValueQuality: TpSCADATagValueState) of object;
+  TpSCADASetDateTimeValue = procedure(const addresInfo:TpSCADAAddressInfo; const Value:TDateTime; var ValueQuality: TpSCADATagValueState) of object;
 
   { TpSCADAPLCAddressInfo }
 
@@ -68,19 +89,20 @@ type
   protected
     procedure AddressChanged; override;
   public
- {0}property MemAddress:LongInt read FMemAddress write SetMemAddress;
- {1}property MemAddressArea:LongInt read FMemAddressArea write SetMemAddressArea;
- {2}property MemAddressAreaForWrite:LongInt read FMemAddressAreaForWrite write SetMemAddressAreaForWrite;
- {3}property MemBitAddress:Byte read FMemBitAddress write SetBitAddress;
- {4}property MemDataType:TpSCADATagDataType read FMemDataType write SetMemDataType;
- {5}property MemFileNumber:LongInt read FMemFileNumber write SetMemFileNumber;
- {6}property MemSize:DWord read FMemSize write SetMemSize;
- {7}property PLCRack:LongInt read FPLCRack write SetPLCRack;
- {8}property PLCSlot:LongInt read FPLCSlot write SetPLCSlot;
- {9}property PLCStation:LongInt read FPLCStation write SetPLCStation;
+    procedure Assign(Source: TpSCADAAddressInfo); override;
+ {0}property  MemAddress:LongInt read FMemAddress write SetMemAddress;
+ {1}property  MemAddressArea:LongInt read FMemAddressArea write SetMemAddressArea;
+ {2}property  MemAddressAreaForWrite:LongInt read FMemAddressAreaForWrite write SetMemAddressAreaForWrite;
+ {3}property  MemBitAddress:Byte read FMemBitAddress write SetBitAddress;
+ {4}property  MemDataType:TpSCADATagDataType read FMemDataType write SetMemDataType;
+ {5}property  MemFileNumber:LongInt read FMemFileNumber write SetMemFileNumber;
+ {6}property  MemSize:DWord read FMemSize write SetMemSize;
+ {7}property  PLCRack:LongInt read FPLCRack write SetPLCRack;
+ {8}property  PLCSlot:LongInt read FPLCSlot write SetPLCSlot;
+ {9}property  PLCStation:LongInt read FPLCStation write SetPLCStation;
   end;
 
-  { TpSCADAArrayElementAddressInfo }
+  { TpSCADAArrayItemAddressInfo }
 
   TpSCADAArrayItemAddressInfo = class(TpSCADAAddressInfo)
   private
@@ -89,6 +111,7 @@ type
   private const
     PropIndex = 1000;
   public
+    procedure Assign(Source: TpSCADAAddressInfo); override;
     property Index:LongInt read FIndex write SetIndex;
   end;
 
@@ -104,9 +127,32 @@ type
     PropDataType = 2000;
     PropOffset   = 2001;
   public
-    property DataType:TpSCADATagDataType read FDataType write SetDataType;
-    property Offset:LongInt read FOffset write SetOffset;
+    procedure Assign(Source: TpSCADAAddressInfo); override;
+    property  DataType:TpSCADATagDataType read FDataType write SetDataType;
+    property  Offset:LongInt read FOffset write SetOffset;
   end;
+
+  { TpSCADATagBitAddressInfo }
+
+  TpSCADATagBitAddressInfo = class(TpSCADAAddressInfo)
+  private
+    FBit: Byte;
+    FBitCount: Byte;
+    FUseValueRaw: Boolean;
+    procedure SetBit(AValue: Byte);
+    procedure SetBitCount(AValue: Byte);
+    procedure SetUseValueRaw(AValue: Boolean);
+  private const
+    PropBit         = 3000;
+    PropBitCount    = 3001;
+    PropUseValueRaw = 3002;
+  public
+    procedure Assign(Source: TpSCADAAddressInfo); override;
+    property  Bit:Byte read FBit write SetBit;
+    property  BitCount:Byte read FBitCount Write SetBitCount;
+    property  UseValueRaw:Boolean read FUseValueRaw write SetUseValueRaw;
+  end;
+
 
   IpSCADAPLCLinkedInterface = interface
   ['{856323E9-BECF-4BBE-B608-64449FB9903A}']
@@ -128,34 +174,270 @@ type
     procedure ValidateInsert(AComponent: TComponent); override;
   end;
 
-  TpSCADATagbookTag = class(TpSCADATagbookItem);
+  { TpSCADATagbookTag }
+
+  TpSCADATagbookTag = class(TpSCADATagbookItem)
+  protected
+    FSyncValueQuality: TpSCADATagValueState;
+    FSyncWriteQuality: TpSCADATagValueState;
+    FValueQuality: TpSCADATagValueState;
+    FWriteQuality: TpSCADATagValueState;
+    FValueTimestamp:TDateTime;
+
+    property ValueQuality:TpSCADATagValueState read FValueQuality;
+    property ValueTimestamp:TDateTime read FValueTimestamp;
+    property WriteQuality:TpSCADATagValueState read FWriteQuality;
+    property SyncValueQuality:TpSCADATagValueState read FSyncValueQuality;
+    property SyncWriteQuality:TpSCADATagValueState read FSyncWriteQuality;
+  end;
 
   TpSCADATagbookFixedSizeTag = class(TpSCADATagbookTag);
 
-  { TpSCADATabgookNumericTag }
+  { TpSCADATagbookNumericTag }
 
   TpSCADATagbookNumericTag = class(TpSCADATagbookFixedSizeTag)
   protected
+    FScale: TpSCADAScaleProcessor;
+    procedure SetScale(AValue: TpSCADAScaleProcessor);
+
     function SwapBytes(const aWord:Word):Word; virtual;
     function SwapWords(const aDWord: DWord): DWord; virtual;
     function SwapDWords(const aQWord: QWord): QWord; virtual;
+    procedure Notification(AComponent: TComponent; Operation: TOperation);
+      override;
+    property Scale:TpSCADAScaleProcessor read FScale write SetScale;
   end;
 
   { TpSCADATagbookIntegerTag }
 
   TpSCADATagbookIntegerTag = class(TpSCADATagbookNumericTag)
+  private
+    function GetValue: Int64;
+    procedure SetValue(AValue: Int64);
   protected
+    FGetValueProc:TpSCADAGetIntegerValue;
+    FSetValueProc:TpSCADASetIntegerValue;
+    function GetValueRaw: Int64; virtual; abstract;
+    procedure SetValueRaw(AValue: Int64); virtual; abstract;
+
+    procedure ReatTagBitValue(const addresInfo:TpSCADAAddressInfo; var Value:Int64;     var ValueTimeStamp:TDateTime; var ValueQuality: TpSCADATagValueState);
+    procedure WritTagBitValue(const addresInfo:TpSCADAAddressInfo; const Value:Int64; var ValueQuality: TpSCADATagValueState);
+
+
     procedure ValidateInsert(AComponent: TComponent); override;
+    property Value:Int64 read GetValue write SetValue;
+    property ValueRaw:Int64 read GetValueRaw write SetValueRaw;
+  public
+    constructor Create(AOwner: TComponent; GetValueProc:TpSCADAGetIntegerValue; SetValueProc:TpSCADASetIntegerValue); virtual;
   end;
 
-  TpSCADATagbookBitmaskTag = class(TpSCADATagbookIntegerTag);
+  { TpSCADATagbookBitmaskTag }
+
+  TpSCADATagbookBitmaskTag = class(TpSCADATagbookIntegerTag)
+  private
+    FIntegerTagLoaded:TpSCADATagbookIntegerTag;
+    function getIntegerTagStored: Boolean;
+  protected
+    FBitAddressInfo:TpSCADATagBitAddressInfo;
+    FIntegerTag: TpSCADATagbookIntegerTag;
+    function GetBitAddress: TpSCADATagBitAddressInfo;
+    function GetIntegerTag: TpSCADATagbookIntegerTag;
+    procedure SetBitAddress(AValue: TpSCADATagBitAddressInfo);
+    function GetValueRaw: Int64; override;
+    procedure SetIntegerTag(AValue: TpSCADATagbookIntegerTag);
+    procedure SetValueRaw(AValue: Int64); override;
+    procedure Notification(AComponent: TComponent; Operation: TOperation); override;
+    procedure Loaded; override;
+  public
+    constructor Create(AOwner: TComponent; GetValueProc: TpSCADAGetIntegerValue; SetValueProc: TpSCADASetIntegerValue); override;
+    destructor Destroy; override;
+
+  published
+    property BitAddress:TpSCADATagBitAddressInfo read GetBitAddress write SetBitAddress;
+    property Scale;
+    property Value;
+    property ValueRaw;
+    property IntegerTag:TpSCADATagbookIntegerTag read GetIntegerTag write SetIntegerTag stored getIntegerTagStored;
+
+  end;
 
 resourcestring
   SpSCADASubComponentsNotAllowed = 'This class %s dont accept %s class as child.';
+  SpSCADAAssignError             = 'Assign error: The class %s cannot assign %s class';
+  SpSCADABitCountError           = 'The bit count must be greater than zero.';
 
 implementation
 
+uses math;
+
+{ TpSCADATagbookBitmaskTag }
+
+function TpSCADATagbookBitmaskTag.getIntegerTagStored: Boolean;
+begin
+  Result:=not (assigned(Owner) and (Owner is TpSCADATagbookIntegerTag));
+end;
+
+function TpSCADATagbookBitmaskTag.GetBitAddress: TpSCADATagBitAddressInfo;
+begin
+  Result:=TpSCADATagBitAddressInfo.Create;
+  Result.Assign(FBitAddressInfo);
+end;
+
+function TpSCADATagbookBitmaskTag.GetIntegerTag: TpSCADATagbookIntegerTag;
+begin
+  if assigned(Owner) and (Owner is TpSCADATagbookIntegerTag) then
+    Result:=Owner as TpSCADATagbookIntegerTag
+  else
+    Result:=FIntegerTag;
+end;
+
+procedure TpSCADATagbookBitmaskTag.SetBitAddress(
+  AValue: TpSCADATagBitAddressInfo);
+begin
+  if Assigned(FBitAddressInfo) then
+    FBitAddressInfo.Assign(AValue);
+end;
+
+function TpSCADATagbookBitmaskTag.GetValueRaw: Int64;
+begin
+  if Assigned(FGetValueProc) then
+    FGetValueProc(FBitAddressInfo, Result, FValueTimestamp, FValueQuality);
+end;
+
+procedure TpSCADATagbookBitmaskTag.SetIntegerTag(
+  AValue: TpSCADATagbookIntegerTag);
+begin
+  if assigned(Owner) and (Owner is TpSCADATagbookIntegerTag) then exit;
+
+  if [csReading, csLoading]*ComponentState<>[] then begin
+    FIntegerTagLoaded:=AValue;
+    exit;
+  end;
+
+  if FIntegerTag=AValue then Exit;
+
+  if Assigned(FIntegerTag) then
+    FIntegerTag.RemoveFreeNotification(Self);
+
+  FIntegerTag:=AValue;
+
+  if Assigned(FIntegerTag) then
+    FIntegerTag.FreeNotification(Self);
+end;
+
+procedure TpSCADATagbookBitmaskTag.SetValueRaw(AValue: Int64);
+begin
+  if Assigned(FSetValueProc) then
+    FSetValueProc(FBitAddressInfo, AValue, FWriteQuality);
+end;
+
+constructor TpSCADATagbookBitmaskTag.Create(AOwner: TComponent;
+  GetValueProc: TpSCADAGetIntegerValue; SetValueProc: TpSCADASetIntegerValue);
+begin
+  inherited Create(AOwner, GetValueProc, SetValueProc);
+  FBitAddressInfo:=TpSCADATagBitAddressInfo.Create;
+  if Assigned(AOwner) and (AOwner is TpSCADATagbookIntegerTag) then begin
+
+  end;
+end;
+
+destructor TpSCADATagbookBitmaskTag.Destroy;
+begin
+  FreeAndNil(FBitAddressInfo);
+  inherited Destroy;
+end;
+
+procedure TpSCADATagbookBitmaskTag.Notification(AComponent: TComponent;
+  Operation: TOperation);
+begin
+  if (Operation=opRemove) and Assigned(FIntegerTag) and (AComponent=FIntegerTag) then begin
+    FIntegerTag:=nil;
+    FGetValueProc:=nil;
+    FSetValueProc:=nil;
+  end else
+    inherited Notification(AComponent, Operation);
+end;
+
+procedure TpSCADATagbookBitmaskTag.Loaded;
+begin
+  inherited Loaded;
+  if getIntegerTagStored then
+    SetIntegerTag(FIntegerTagLoaded);
+end;
+
+{ TpSCADATagBitAddressInfo }
+
+procedure TpSCADATagBitAddressInfo.SetBit(AValue: Byte);
+begin
+  if FBit=AValue then Exit;
+  DoChangeValidation(PropBit,FBit, AValue);
+  FBit:=AValue;
+  AddressChanged;
+end;
+
+procedure TpSCADATagBitAddressInfo.SetBitCount(AValue: Byte);
+begin
+  if FBitCount=AValue then Exit;
+  if AValue<=0 then
+    raise exception.Create(SpSCADABitCountError);
+  DoChangeValidation(PropBitCount, FBitCount, AValue);
+  FBitCount:=AValue;
+  AddressChanged;
+end;
+
+procedure TpSCADATagBitAddressInfo.SetUseValueRaw(AValue: Boolean);
+begin
+  if FUseValueRaw=AValue then Exit;
+  DoChangeValidation(PropUseValueRaw, ifthen(FUseValueRaw,1,0), ifthen(AValue, 1, 0));
+  FUseValueRaw:=AValue;
+  AddressChanged;
+end;
+
+procedure TpSCADATagBitAddressInfo.Assign(Source: TpSCADAAddressInfo);
+var
+  aSource: TpSCADATagBitAddressInfo;
+begin
+  if Assigned(Source) and (Source is TpSCADATagBitAddressInfo) then begin
+    aSource:=TpSCADATagBitAddressInfo(Source);
+    FBit      :=asource.FBit;
+    FBitCount :=asource.FBitCount;
+  end else
+    inherited Assign(Source);
+end;
+
 { TpSCADATagbookIntegerTag }
+
+function TpSCADATagbookIntegerTag.GetValue: Int64;
+begin
+  if Assigned(FScale) then
+    Result := Trunc(FScale.SetPLCValueGetSysValue(Self, GetValueRaw))
+  else
+    Result:=GetValueRaw;
+end;
+
+procedure TpSCADATagbookIntegerTag.SetValue(AValue: Int64);
+begin
+  if Assigned(FScale) then
+    SetValueRaw(Trunc(FScale.SetSysValueGetPLCValue(Self, AValue)))
+  else
+    SetValueRaw(AValue);
+end;
+
+procedure TpSCADATagbookIntegerTag.ReatTagBitValue(
+  const addresInfo: TpSCADAAddressInfo; var Value: Int64;
+  var ValueTimeStamp: TDateTime; var ValueQuality: TpSCADATagValueState);
+begin
+  if not (addresInfo is TpSCADATagBitAddressInfo) then
+    raise Exception.Create();
+end;
+
+procedure TpSCADATagbookIntegerTag.WritTagBitValue(
+  const addresInfo: TpSCADAAddressInfo; const Value: Int64;
+  var ValueQuality: TpSCADATagValueState);
+begin
+  if not (addresInfo is TpSCADATagBitAddressInfo) then
+    raise Exception.Create();
+end;
 
 procedure TpSCADATagbookIntegerTag.ValidateInsert(AComponent: TComponent);
 begin
@@ -163,7 +445,25 @@ begin
   raise Exception.Create(Format(SpSCADASubComponentsNotAllowed,[Self.ClassName, AComponent.ClassName]));
 end;
 
+constructor TpSCADATagbookIntegerTag.Create(AOwner: TComponent;
+  GetValueProc: TpSCADAGetIntegerValue; SetValueProc: TpSCADASetIntegerValue);
+begin
+  inherited Create(AOwner);
+  FSetValueProc:=SetValueProc;
+  FGetValueProc:=GetValueProc;
+end;
+
 { TpSCADATabgookNumericTag }
+
+procedure TpSCADATagbookNumericTag.SetScale(AValue: TpSCADAScaleProcessor);
+begin
+  if FScale=AValue then Exit;
+  if Assigned(FScale) then
+    FScale.RemoveFreeNotification(Self);
+  if Assigned(AValue) then
+    AValue.FreeNotification(Self);
+  FScale:=AValue;
+end;
 
 function TpSCADATagbookNumericTag.SwapBytes(const aWord: Word): Word;
 var
@@ -196,6 +496,14 @@ begin
   aux:=dwords[1];
   dwords[1]:=dwords[0];
   dwords[0]:=aux;
+end;
+
+procedure TpSCADATagbookNumericTag.Notification(AComponent: TComponent;
+  Operation: TOperation);
+begin
+  inherited Notification(AComponent, Operation);
+  if (Operation=opRemove) and (AComponent=FScale) then
+    FScale:=nil;
 end;
 
 { TpSCADATagbookItem }
@@ -233,6 +541,18 @@ begin
   AddressChanged;
 end;
 
+procedure TpSCADAStructItemAddressInfo.Assign(Source: TpSCADAAddressInfo);
+var
+  aSource: TpSCADAStructItemAddressInfo;
+begin
+  if Assigned(Source) and (Source is TpSCADAStructItemAddressInfo) then begin
+    aSource:=TpSCADAStructItemAddressInfo(Source);
+    FDataType:=asource.FDataType;
+    FOffset  :=asource.FOffset;
+  end else
+    inherited Assign(Source);
+end;
+
 { TpSCADAArrayElementAddressInfo }
 
 procedure TpSCADAArrayItemAddressInfo.SetIndex(AValue: LongInt);
@@ -243,7 +563,20 @@ begin
   AddressChanged;
 end;
 
+procedure TpSCADAArrayItemAddressInfo.Assign(Source: TpSCADAAddressInfo);
+begin
+  if Assigned(Source) and (Source is TpSCADAArrayItemAddressInfo) then
+    FIndex := TpSCADAArrayItemAddressInfo(Source).FIndex
+  else
+    inherited Assign(Source);
+end;
+
 { TpSCADAAddressInfo }
+
+procedure TpSCADAAddressInfo.Assign(Source: TpSCADAAddressInfo);
+begin
+  raise exception.Create(Format(SpSCADAAssignError,[ClassName, Source.ClassName]));
+end;
 
 procedure TpSCADAAddressInfo.DoChangeValidation(PropertyNumber: Word; OldValue,
   NewValue: Int64);
@@ -337,6 +670,26 @@ end;
 procedure TpSCADAPLCAddressInfo.AddressChanged;
 begin
 
+end;
+
+procedure TpSCADAPLCAddressInfo.Assign(Source: TpSCADAAddressInfo);
+var
+  aSource: TpSCADAPLCAddressInfo;
+begin
+  if Assigned(Source) and (Source is TpSCADAPLCAddressInfo) then begin
+    aSource:=TpSCADAPLCAddressInfo(Source);
+    FMemAddress             := aSource.FMemAddress;
+    FMemAddressArea         := aSource.FMemAddressArea;
+    FMemAddressAreaForWrite := aSource.FMemAddressAreaForWrite;
+    FMemBitAddress          := aSource.FMemBitAddress;
+    FMemDataType            := aSource.FMemDataType;
+    FMemFileNumber          := aSource.FMemFileNumber;
+    FMemSize                := aSource.FMemSize;
+    FPLCRack                := aSource.FPLCRack;
+    FPLCSlot                := aSource.FPLCSlot;
+    FPLCStation             := aSource.FPLCStation;
+  end else
+    inherited Assign(Source);
 end;
 
 { TpSCADATagbookFolder }
